a6eac822bd1019d959ee8ce2d23990d777656de2
lwalker <landen@example.com>
2023-10-23 07:29:48 -0500

    first commit

---
+++ app/index.js
@@ -0,0 +1,12 @@
+// View = div tag and Text = p tag but in only react-native
+import { View, Text } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home;
